520f630612a222f24b12bc4b5d128bff29f88515
Adam Gallard <adam@thegallards.com>
2025-07-18 12:09:16 +1000

    update to loginparents API

--- scoutbase.client/api/loginParent.js
+++ scoutbase.client/api/loginParent.js
@@ -13,7 +13,9 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    if (req.method !== 'POST') return res.status(405).end();
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
 
     const { identifier, enteredPin, groupId } = req.body;
 
